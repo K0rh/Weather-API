3cd127bfd41086c02f606a6d268ff8d3c8586562
K0rh <111435820+K0rh@users.noreply.github.com>
2022-10-30 17:39:02 +0100

    tsconfig + basic fetch function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cityName, setCityName] = useState("London");
+  const apiKey = "e5a7da137bfa07cb609ceb9e8957b235";
+
+  fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=5&appid="+apiKey)
+        .then(response => response.json())
+        .then(data =>  console.log(data)); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="searchForm">
+        <label>
+          Ville : <input />
+        </label>
+      </form>
+      <button className="searchButton">Search</button>
+
+      
     </div>
   );
 }
