499d441ff550d3d4c2e52008a1de0630a34e47fe
K0rh <111435820+K0rh@users.noreply.github.com>
2022-11-07 18:56:29 +0100

    Updates

---
+++ App.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./App.css");
+const react_1 = __importDefault(require("react"));
+function App() {
+    return (<div className="App">
+      <form>
+        <label>
+          <input>
+          Ville :
+          </input>
+        </label>
+      </form>
+    </div>);
+}
+exports.default = App;
